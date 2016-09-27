384acbcf81922674908cb8073b6f084ad01d7bdf
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-09-26 23:52:05 -0300

    Versionamento inicial do projeto

---
+++ elctrl/src/main/java/br/com/qualityfactory/el/elctrl/VerbController.java
@@ -0,0 +1,14 @@
+package br.com.qualityfactory.el.elctrl;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Result;
+
+@Controller
+public class VerbController {
+	
+	@Inject
+	private Result result;
+
+}
---
+++ elctrl/src/test/java/br/com/qualityfactory/el/elctrl/test/VerbTestController.java
@@ -0,0 +1,18 @@
+package br.com.qualityfactory.el.elctrl.test;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+
+public class VerbTestController {
+	
+	@Inject
+	private VerbTestController controller;
+	
+	@Test
+	public void getAllVerbs() throws Exception{
+		//List<VerbModel> controller.getAllVerbs();
+	}
+}
